b99a83a6ebdae659082015174711431e99fde803
KARUNAKAR <KARUNAKAR@192.168.0.52>
2018-12-02 16:34:04 +0530

    2nd change

--- src/first1/Employee.java
+++ src/first1/Employee.java
@@ -3,11 +3,11 @@ package first1;
 import java.text.SimpleDateFormat;
 import java.util.Random;
 public class Employee {
-public static void main(String[] args) {
- String str="ORA";
- SimpleDateFormat dte=new SimpleDateFormat("yyyy-mm-dd");
- int rn=new Random().nextInt(1000);
- System.out.println(str+"-"+dte+"-"+rn);
- System.out.println();
-}
+	public static void main(String[] args) {
+		String str="ORA";
+		SimpleDateFormat dte=new SimpleDateFormat("yyyy-mm-dd");
+		int rn=new Random().nextInt(1000);
+		System.out.println(str+"-"+dte+"-"+rn);
+		System.out.println();
+	}
 }
